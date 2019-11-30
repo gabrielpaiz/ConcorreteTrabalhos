77a486d87846b17723af815997d30fab08b45c13
Gabriel <gabriel.justo@acad.pucrs.br>
2019-11-30 13:55:00 -0300

    o processo mestre est'a mandando o hist'orico mas o novo processo s'o recebe WHI

--- TF_Concorrente_Chat/chat.go
+++ TF_Concorrente_Chat/chat.go
@@ -13,7 +13,7 @@ import (
 
 //Send from BEB
 //Função que Faz o Envio de Mensagem
-func Send(block chan struct{}, module BEB.BestEffortBroadcast_Module, ads *[]string) {
+func Send(module BEB.BestEffortBroadcast_Module, ads *[]string, messages *[]string) {
 	for {
 
 		reader := bufio.NewReader(os.Stdin)
@@ -24,13 +24,14 @@ func Send(block chan struct{}, module BEB.BestEffortBroadcast_Module, ads *[]str
 			Message:   message}
 
 		module.Req <- sendMessage
+		*messages = append((*messages), ((*ads)[0] + ":"+ sendMessage.Message))
 	}
 
 }
 
 //Recv from BEB
 //Função que Faz Recebimento da Mensagem
-func Recv(module BEB.BestEffortBroadcast_Module, ads *[]string) {
+func Recv(module BEB.BestEffortBroadcast_Module, ads *[]string, messages *[]string) {
 	for {
 		recvMessage := <-module.Ind
 		aux := true
@@ -40,7 +41,6 @@ func Recv(module BEB.BestEffortBroadcast_Module, ads *[]string) {
 			//fmt.Println("mesage[1:]: ", recvMessage.Message[2:])
 			//fmt.Println("ads[i]: ", (*ads)[i])
 			if (*ads)[i] == recvMessage.Message[2:] {
-				fmt.Println("entrei no if")
 				aux = false
 			}
 		}
@@ -49,32 +49,46 @@ func Recv(module BEB.BestEffortBroadcast_Module, ads *[]string) {
 		if recvMessage.Message[0:2] == "PP" && aux {
 			//fmt.Println("Teste")
 			*ads = append((*ads), recvMessage.Message[2:])
-			fmt.Println("ads: ",(*ads))
+
 			sendPara := BEB.BestEffortBroadcast_Req_Message{
 				Addresses: (*ads)[1:],
 				Message:   recvMessage.Message}
 			module.Req <- sendPara
-		}
-		//fmt.Println("\n\nPorteiro: *ads[0] ", (*ads)[len(*ads)-1], "\n\n")
 
-		fmt.Println(recvMessage.From, ": ", recvMessage.Message)
+			fmt.Println("len: ", len(*messages))
+			for i := 0; i < len(*messages); i++{
+				fmt.Println("entri no for do historico")
+				sendHist := BEB.BestEffortBroadcast_Req_Message{
+					Addresses: (*ads)[len((*ads))-1 : len((*ads))],
+					Message: (*messages)[i]}
+
+				module.Req <- sendHist
+
+				//time.Sleep(100 * time.Millisecond)
+				//for i:=0; i<500; i++{}
+			}
+		}else{
+			*messages = append(*messages, (recvMessage.From + ":" + recvMessage.Message))
+			fmt.Println(recvMessage.From, ": ", recvMessage.Message)
+		}
 	}
 
 }
 
 // Funcção de entrada
 func join(module BEB.BestEffortBroadcast_Module, ads []string) {
-	//message := "\n" + ads[0] + " entrou no chat\n"
+	message := "\n" + ads[0] + " entrou no chat\n"
 
 	perm := "PP" + ads[0]
-	/*sendMessage := BEB.BestEffortBroadcast_Req_Message{
+	sendMessage := BEB.BestEffortBroadcast_Req_Message{
 		Addresses: ads[1:],
 		Message:   message}
 
 	module.Req <- sendMessage
 
-	time.Sleep( 50* time.Millisecond)
-*/
+	
+	for i:=0; i<500; i++{} //Evita que as duas mensagens sejam enviadas juntas (com sleep n~ao funciona)
+
 	sendPermission := BEB.BestEffortBroadcast_Req_Message{
 		Addresses: ads[1:2],
 		Message:   perm}
@@ -102,12 +116,12 @@ func main() {
 
 	beb.Init(users[0])
 
-	block := make(chan struct{})
 
 	join(beb, users)
 
-	go Send(block, beb, &users)
-	go Recv(beb, &users)
+	var messages [] string
+	go Send(beb, &users, &messages)
+	go Recv(beb, &users, &messages)
 
 	for {
 		time.Sleep(2 * time.Second)
